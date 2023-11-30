89f3cd9a98389f50ff8265ced216b2a4abdfa67b
Tamer <tamer.masud@mail.ru>
2023-11-30 14:39:22 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FinalTask
+{
+    [Serializable]
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:/Users/tamer/Рабочий стол/Students.dat";
+            string directoryPath = @"C:/Users/tamer/Рабочий стол/Students";
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"Фаил {filePath} не найден");
+                return;
+            }
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+            using (FileStream fs = new FileStream(filePath, FileMode.Open))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                Student[] students = (Student[])formatter.Deserialize(fs);
+
+                foreach (Student student in students)
+                {
+                    string groupFilePath = Path.Combine(directoryPath, student.Group + ".txt");
+
+                    using (StreamWriter sw = new StreamWriter(groupFilePath, true))
+                    {
+                        sw.WriteLine($"{student.Name}, {student.DateOfBirth.ToShortDateString()}");
+                    }
+                }
+            }
+
+            Console.WriteLine("Информация о студентах была составлена");
+        }
+    }
+}
